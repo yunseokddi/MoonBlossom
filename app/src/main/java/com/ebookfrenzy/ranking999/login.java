56bb40c54c9ef31da63eedb9b6883f8f3d593bf1
yunseokddi <39209028+yunseokddi@users.noreply.github.com>
2019-01-05 22:56:25 +0900

    first commit

---
+++ app/src/main/java/com/ebookfrenzy/ranking999/login.java
@@ -0,0 +1,13 @@
+package com.ebookfrenzy.ranking999;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class login extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
